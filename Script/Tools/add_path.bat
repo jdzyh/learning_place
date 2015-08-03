eef4c020f99cf9f3f4a7729899b9c5c8a36bc253
jdzyh <dtzyhy@163.com>
2015-08-03 16:11:03 +0800

    2015.8.3 small edit on .bat

--- Script/Tools/add_path.bat
+++ Script/Tools/add_path.bat
@@ -1,7 +1,7 @@
 @echo off &setlocal enabledelayedexpansion
 
 ::作为参数传入:path_dst
-ECHO $ ----START----
+ECHO [+] ----START----
 
 for /f "tokens=1,2*" %%a in (
 	'reg query "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" /v Path' 
@@ -13,38 +13,38 @@ for /f "tokens=1,2*" %%a in (
 set path_split_right=%path_all%
 call :Loop_split
 
-ECHO $ Input dst_path:
+ECHO [+] Input dst_path:
 set /p path_dst=
-ECHO $ !path_dst!
+ECHO [+] !path_dst!
 
-echo $ Current Path :
-echo $	"!path_all!" |find /i "!path_dst!" && set FindPath=true || set FindPath=false &echo $	!path_all!
-echo $
-echo $ FindPath ? : !FindPath!
+echo [+] Current Path :
+echo [+]	"!path_all!" |find /i "!path_dst!" && set FindPath=true || set FindPath=false &echo [+]	!path_all!
+echo [+]
+echo [+] FindPath ? : !FindPath!
 
 if /i !FindPath!==true (
-	echo $ Path Already Exists!
+	echo [+] Path Already Exists!
 
 ) else (
-	echo $ Sure To Add New Path ? [Y/N]
+	echo [+] Sure To Add New Path ? [Y/N]
 	set /p ans=
 	
 	if /i ans==Y (
 		reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%path_all%;%path_dst%" /f
-		echo $New PATH : 
+		echo [+]New PATH : 
 		path
 	)
 )
-echo $ EXIT
-echo $	
-echo $	
-echo $	
-echo $	
+echo [+] EXIT
+echo [+]	
+echo [+]	
+echo [+]	
+echo [+]	
 goto :eof
 
 :Loop_split
 for /f "delims=; tokens=1*" %%c in ("%path_split_right%") do (
-	echo $	%%c
+	echo [+]	%%c
 	if %%d==nul (
 		goto Loop_split_End
 	) else (
