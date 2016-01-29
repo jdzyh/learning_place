2db4f1b1a21784509c80d0ac5d218b0271e7eced
jdzyh <dtzyhy@163.com>
2016-01-29 15:03:14 +0800

    2016.1.29.new init

--- Script/Learning/move.bat
+++ Script/Learning/move.bat
@@ -1,17 +1,17 @@
-@echo START
-@pause
-
-@move ".\*.ass" "..\..\..\[Ghost in the Shell S.A.C.2nd GIG攻壳机动队TV版第二季][X264 1920x1080_AAC][Raw][BDRip]\"
-
-@echo END
-@pause
-
-REM @echo off
-REM for /f "delims=" %%a in ('dir /b/s/a-d "*(BD 1920x1080 x264 AAC 日本語2ch+日本語5.1ch+英語5.1ch).ass"') do (
-    REM @set "str=%%~nxa"
-    REM setlocal enabledelayedexpansion
-    REM ren "%%a" "!str:~0,-51!.ass"
-    REM endlocal
-REM )
-
+REM @echo START
 REM @pause
+
+REM @move ".\*.ass" "..\..\..\[Ghost in the Shell S.A.C.2nd GIG攻壳机动队TV版第二季][X264 1920x1080_AAC][Raw][BDRip]\"
+
+REM @echo END
+REM @pause
+
+@echo off
+for /f "delims=" %%a in ('dir /b/s/a-d "陈明*.jpg"') do (
+    @set "str=%%~nxa"
+    setlocal enabledelayedexpansion
+    ren "%%a" "!str:2,0!.ass"
+    endlocal
+)
+
+@pause
